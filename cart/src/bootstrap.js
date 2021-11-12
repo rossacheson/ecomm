5541ccb5208e93ca8658ecf215a72675b713010e
Ross Acheson <ross.acheson@gmail.com>
2021-11-12 13:49:30 -0500

    mount cart as well

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,5 +1,21 @@
 import faker from 'faker';
 
-const cartText = `<div>You have ${faker.datatype.number(50)} items in your cart</div>`;
+const mount = (el) => {
+    const cartText = `<div>You have ${faker.datatype.number(50)} items in your cart</div>`;
 
-document.querySelector('#dev-cart').innerHTML = cartText;
+    el.innerHTML = cartText;
+};
+
+// for dev in isolation
+if (process.env.NODE_ENV === 'development') {
+    const el = document.querySelector('#dev-cart');
+
+    // assuing our container doesn't have an element with id 'dev-products', and dev does
+    if (el) {
+        // we are probably running in isolaiton
+        mount(el);
+    }
+}
+
+// for container to use
+export { mount };
