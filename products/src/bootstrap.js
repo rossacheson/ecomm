4b86a81b38cc5c34e41cc90a57097b737deaecda
Ross Acheson <ross.acheson@gmail.com>
2021-11-12 12:38:00 -0500

    Use/export a mount function instead of just looking for a hardcoded div

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,26 @@
 import faker from 'faker';
 
-let products = '';
+const mount = (el) => {
+    let products = '';
 
-for (let i = 0; i < 5; i++) {
-    const name = faker.commerce.productName();
-    products += `<div>${name}</div>`;
+    for (let i = 0; i < 5; i++) {
+        const name = faker.commerce.productName();
+        products += `<div>${name}</div>`;
+    }
+
+    el.innerHTML = products;
+};
+
+// for dev in isolation
+if (process.env.NODE_ENV === 'development') {
+    const el = document.querySelector('#dev-products');
+
+    // assuing our container doesn't have an element with id 'dev-products', and dev does
+    if (el) {
+        // we are probably running in isolaiton
+        mount(el);
+    }
 }
 
-document.querySelector('#dev-products').innerHTML = products;
+// for container to use
+export { mount };
